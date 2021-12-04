fbf44861a65b887b0ac32eaf64213817d737c002
yoonmin-kim <rladbsals23@gmail.com>
2021-12-04 19:05:15 +0900

    Initial commit

---
+++ src/main/java/com/hello/jwttut/JwtTutApplication.java
@@ -0,0 +1,13 @@
+package com.hello.jwttut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTutApplication.class, args);
+	}
+
+}
